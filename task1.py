2b649eb6806051fd8ad6e64c3be5fdb6df957ed6
rakshitha212 <101247158+rakshitha212@users.noreply.github.com>
2025-05-18 21:51:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+print("Hello! My name is rakshitha, welcome to my repo.")
+""" 
+print is a command used in Python to show messages on the output screen.
+The text inside the quotes is called a string.
+It's just a group of characters that we want to display.
+When you put the string inside the parentheses of the print command, 
+Python shows it as output."""
